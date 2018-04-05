f6ff3f39d7e88567a0ef5e1c35988bc4c484ff93
Glitch (bloc-todo-app) <none>
2018-04-05 17:28:22 +0000

    Simple To-Do: DOM Scripting - Events, Part 2 (assignment)

--- app.js
+++ app.js
@@ -1,62 +1,58 @@
 function onReady() {
+  const toDos = [];
   const addToDoForm = document.getElementById('addToDoForm');
-  const newToDoText = document.getElementById('newToDoText');
-  const toDoList = document.getElementById('toDoList');
   
-  addToDoForm.addEventListener('submit', event => {
-    event.preventDefault();
-    // get the text
-    let title = newToDoText.value;
-
-    // create a new li
-    let newLi = document.createElement('li');
-
-    // create a new input
-    let checkbox = document.createElement('input');
+  function createNewToDo() {
+    // access text input 
+    const newToDoText = document.getElementById('newToDoText');
     
-    // Add a delete button which the user can use to delete to-dos they no longer need.
-    let deleteButton = document.createElement('button');
+    // if newToDoText is empty 
+    if (!newToDoText.value) { return; }
     
-    // Add text to deleteButton
-    deleteButton.textContent = "x";
-
-    // set the input's type to checkbox
-    checkbox.type = "checkbox";
-    
-    // set the text of the todo
-    newLi.textContent = title;
-
-    // add the checkbox to the li
-    newLi.appendChild(checkbox);
-    
-    // add the deleteButton to newLi
-    newLi.appendChild(deleteButton);
-    
-    // add the li to the ul
-    toDoList.appendChild(newLi);
-    
-    // empty the input
-    newToDoText.value = '';
-    
-    // delete todo when deleteButton is clicked
-    
-    
-    // Bloc's suggested solution 
-    // deleteButton.addEventListener('click', () => {
-    //  newLi.parentNode.removeChild(newLi);
-    // });
-    
-    // Regular function expression solution with this
-    // deleteButton.addEventListener('click', function() {
-    //   this.parentNode.remove();
-    // });
-    
-    // es6 arrow function using event.target
-    deleteButton.addEventListener('click', event => {     
-      event.target.parentNode.remove();
+    // push value of newToDoText to array
+    toDos.push({
+      title: newToDoText.value,
+      complete: false
     });
     
+    // clear text input
+    newToDoText.value = '';
+    
+    renderTheUI();
+  }
+  
+  function renderTheUI() {
+    // access the todolist UL
+    const toDoList = document.getElementById('toDoList');
+    
+    // set new li to blank
+    toDoList.textContent = '';
+    
+    // for each
+    toDos.forEach(function(toDo) {
+      // create LI
+      const newLi = document.createElement('li');
+      // create checkbox
+      const checkbox = document.createElement('input');
+      // make checkbox a checkbox
+      checkbox.type = "checkbox";
+      
+      // set text
+      newLi.textContent = toDo.title;
+      
+      // append li and checkbox
+      toDoList.appendChild(newLi);
+      newLi.appendChild(checkbox);
+      
+    });
+  }
+  
+  // run stuff when the form is submitted
+  addToDoForm.addEventListener('submit', event => {
+    event.preventDefault();
+    createNewToDo();
   });
+
 }
 
 window.onload = function() {
